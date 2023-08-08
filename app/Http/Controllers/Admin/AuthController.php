6b99d8f2a08c853e4f0fb91ce3edd5630f211494
Yadan <forwelll@163.com>
2023-08-08 18:37:22 +0800

    inherit

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
         $token = $user->createToken($user->email);
 
         $data = array_merge($user, [
-            'access_token' => $token->plainTextToken,
+            'token' => $token->plainTextToken,
             'expires' => config('sanctum.expiration'),
         ]);
         return response()->json($data);
